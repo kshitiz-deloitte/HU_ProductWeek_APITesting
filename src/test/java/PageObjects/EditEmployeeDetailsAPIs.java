3516e8e5784cba680c10bc50f329fad79c926356
kshitiz-deloitte <101085691+kshitiz-deloitte@users.noreply.github.com>
2022-04-27 18:03:52 +0530

    Properties file and test for Edit employee details added

--- src/test/java/PageObjects/EditEmployeeDetailsAPIs.java
+++ src/test/java/PageObjects/EditEmployeeDetailsAPIs.java
@@ -1,5 +1,52 @@
 package PageObjects;
 
-public class EditEmployeeDetailsAPIs {
+import PreRequisites.BaseClass;
+import io.restassured.response.Response;
 
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class EditEmployeeDetailsAPIs extends BaseClass {
+
+    public Response getResponseFromEditEmployeePersonalDetails(){
+        File jsonFile = new File(properties.getProperty("new_personal_detail_path"));
+        return executePutAndGetResponse(properties.getProperty("edit_personal_details_path"), jsonFile);
+    }
+
+    public Response getResponseFromEditEmployeeBankDetails(){
+        File jsonFile = new File(properties.getProperty("new_personal_detail_path"));
+        return executePutAndGetResponse(properties.getProperty("edit_bank_details_path"), jsonFile);
+    }
+
+    public Response getResponseFromEditEmployeeEducationDetails(){
+        File jsonFile = new File(properties.getProperty("new_personal_detail_path"));
+        return executePutAndGetResponse(properties.getProperty("edit_education_details_path"), jsonFile);
+    }
+
+    public Response getResponseFromEditEmployeeEmploymentDetails(){
+        File jsonFile = new File(properties.getProperty("new_personal_detail_path"));
+        return executePutAndGetResponse(properties.getProperty("edit_employment_history_path"), jsonFile);
+    }
+
+    public Response getResponseFromEditEmployeeContactDetails(){
+        File jsonFile = new File(properties.getProperty("new_personal_detail_path"));
+        return executePutAndGetResponse(properties.getProperty("edit_contact_details_path"),jsonFile);
+    }
+
+    private Response executePutAndGetResponse(String path, File file){
+        requestSpecification.header("Authorization",
+                "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbWFuMTIzIiwiaWF0IjoxNjUxMDU4NDA5LCJleHAiOjE2NTExNDQ4MDl9.mEfIiGjDTkjzffBg33Yc5a7_7BSqdK-TtzuNJS6flJrynNPoMJ1hhrasIhR3iQkQM77rHWNTELzBSfL0GG81hg");
+        requestSpecification.body(file);
+        return given().
+                spec(requestSpecification).
+                when().
+                put(path).
+                then().
+                spec(responseSpecification).
+                extract().
+                response();
+    }
 }
+
+
